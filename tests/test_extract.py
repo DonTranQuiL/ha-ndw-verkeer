bfdb016f7fe924b51c631a8fd142838fb3035e54
malosaaa <90527254+malosaaa@users.noreply.github.com>
2026-09-18 23:17:19 +0000

    style: auto-fix ruff linting and formatting [skip ci]

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -32,9 +32,9 @@ def _load_coordinator_module():
         mod.__path__ = []
         sys.modules[name] = mod
 
-    sys.modules[
-        "homeassistant.helpers.aiohttp_client"
-    ].async_get_clientsession = lambda hass: None
+    sys.modules["homeassistant.helpers.aiohttp_client"].async_get_clientsession = (
+        lambda hass: None
+    )
 
     class _DataUpdateCoordinator:
         def __init__(self, *args, **kwargs):
